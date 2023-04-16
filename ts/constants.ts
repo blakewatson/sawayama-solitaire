9b42832b90428d2a2dbd2f94dc81a6a60eb30ff7
Blake Watson <blake@blakewatson.com>
2023-04-16 12:42:09 -0500

    make everything larger

--- ts/constants.ts
+++ ts/constants.ts
@@ -1,13 +1,13 @@
 // Cards
-export const BOARD_Y = 165;
-export const CARD_W = 85;
+export const CARD_W = 100;
 export const CARD_H = Math.round(CARD_W * 1.38095238);
-export const CARD_OFFSET_VERTICAL = 24;
+export const CARD_OFFSET_VERTICAL = CARD_H / 5;
 export const CARD_OFFSET_HORIZONTAL = 21;
 export const STACK_GAP = 18;
 export const ACE_TRAY_W = CARD_W + STACK_GAP * 2;
 export const DECK_POS = { x: ACE_TRAY_W + STACK_GAP, y: 35 };
 export const CARD_ANIM_SPEED_MS = 100;
+export const BOARD_Y = STACK_GAP + STACK_GAP / 2 + CARD_H + STACK_GAP;
 
 export enum Suit {
   Clubs = 'clubs',
@@ -34,7 +34,7 @@ export enum Rank {
 
 // View
 export const VIEW_W = ACE_TRAY_W + (CARD_W + STACK_GAP) * 7 + STACK_GAP;
-export const VIEW_H = 600;
+export const VIEW_H = (STACK_GAP + CARD_H) * 4 + STACK_GAP * 3;
 
 // Colors
 export const COLOR_BG = '#0f62b5';
